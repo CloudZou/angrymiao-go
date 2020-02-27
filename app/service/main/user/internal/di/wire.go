ee910d857dc20953aee4d345f986a9b86f3b8dbe
CloudZou <Cloud.Zou@quest.com>
2020-02-27 13:39:39 +0800

    app/user

---
+++ app/service/main/user/internal/di/wire.go
@@ -0,0 +1,18 @@
+// +build wireinject
+// The build tag makes sure the stub is not built in the final build.
+
+package di
+
+import (
+	"angrymiao-go/app/service/main/user/internal/dao"
+	"angrymiao-go/app/service/main/user/internal/server/grpc"
+	"angrymiao-go/app/service/main/user/internal/server/http"
+	"angrymiao-go/app/service/main/user/internal/service"
+
+	"github.com/google/wire"
+)
+
+//go:generate punk t wire
+func InitApp() (*App, func(), error) {
+	panic(wire.Build(dao.Provider, service.Provider, http.New, grpc.New, NewApp))
+}
