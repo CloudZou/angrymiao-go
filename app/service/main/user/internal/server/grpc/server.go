90b771b395b0b3f39a94f559f2a1c76da8620bbf
Cloud.Zou <Cloud.Zou@quest.com>
2020-03-11 16:46:21 +0800

    dapper

--- app/service/main/user/internal/server/grpc/server.go
+++ app/service/main/user/internal/server/grpc/server.go
@@ -1,11 +1,10 @@
 package grpc
 
 import (
-	pb "angrymiao-go/app/service/main/user/api"
+	"angrymiao-go/app/service/main/user/api"
 	"angrymiao-go/app/service/main/user/conf"
 	"angrymiao-go/app/service/main/user/internal/service"
 	"context"
-	"github.com/golang/protobuf/ptypes/empty"
 
 	"github.com/CloudZou/punk/pkg/net/rpc/warden"
 )
@@ -13,7 +12,7 @@ import (
 // New new a grpc rpc.
 func New(s *service.Service) (ws *warden.Server, err error) {
 	ws = warden.NewServer(conf.Conf.GRPCServer)
-	pb.RegisterDemoServer(ws.Server(), &rpc{as: s})
+	api.RegisterGreeterServer(ws.Server(), &rpc{as: s})
 	if ws, err = ws.Start(); err != nil {
 		return
 	}
@@ -24,19 +23,9 @@ type rpc struct {
 	as *service.Service
 }
 
-var _ pb.DemoServer = &rpc{}
+var _ api.GreeterServer = &rpc{}
 
-func (s *rpc) Ping(ctx context.Context, req *empty.Empty) (reply *empty.Empty, err error) {
-	reply, err = s.as.Ping(ctx, req)
-	return nil, nil
-}
-
-func (s *rpc) SayHello(ctx context.Context, req *pb.HelloReq) (reply *empty.Empty, err error) {
+func (s *rpc) SayHello(ctx context.Context, req *api.HelloRequest) (reply *api.HelloReply, err error) {
 	reply, err = s.as.SayHello(ctx, req)
-	return
-}
-
-func (s *rpc) SayHelloURL(ctx context.Context, req *pb.HelloReq) (resp *pb.HelloResp, err error) {
-	resp, err = s.as.SayHelloURL(ctx, req)
 	return
 }
