3662d8bcae3c98105dc7d003f6f86fe681e6e426
cloudzou <cloudzou@angrymiao.com>
2020-04-03 17:25:36 +0800

    后台管理系统统一登陆服务（目前比较简单，没有用作rbac权限体系）

---
+++ app/admin/passport/cmd/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"angrymiao-go/app/admin/passport/conf"
+	"angrymiao-go/app/admin/passport/server/http"
+	"angrymiao-go/app/admin/passport/service"
+	"flag"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"angrymiao-go/app/admin/passport/internal/di"
+	"angrymiao-go/punk/conf/paladin"
+	"angrymiao-go/punk/log"
+)
+
+func main() {
+	flag.Parse()
+	if err := conf.Init(); err != nil {
+		log.Error("conf.Init() error(%v)", err)
+		panic(err)
+	}
+	log.Init(nil) // debug flag: log.dir={path}
+	defer log.Close()
+	log.Info("passport start")
+	s, closeFunc, err := service.New(conf.Conf)
+	http.New(conf.Conf, s)
+	if err != nil {
+		panic(err)
+	}
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, syscall.SIGHUP, syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGINT)
+	for {
+		s := <-c
+		log.Info("get a signal %s", s.String())
+		switch s {
+		case syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGINT:
+			closeFunc()
+			log.Info("passport exit")
+			time.Sleep(time.Second)
+			return
+		case syscall.SIGHUP:
+		default:
+			return
+		}
+	}
+}
---
+++ app/admin/passport/conf/conf.go
@@ -0,0 +1,76 @@
+package conf
+
+import (
+	"angrymiao-go/punk/conf"
+	"angrymiao-go/punk/database/orm"
+	"angrymiao-go/punk/log"
+	bm "angrymiao-go/punk/net/http/blademaster"
+	"angrymiao-go/punk/net/rpc/warden"
+	"errors"
+	"flag"
+	"github.com/BurntSushi/toml"
+)
+
+var (
+	confPath string
+	client   *conf.Client
+	// Conf config
+	Conf = &Config{}
+)
+
+type Config struct {
+	Server *bm.ServerConfig
+	DB     *orm.Config
+	WardenServer *warden.ServerConfig
+}
+
+func init() {
+	flag.StringVar(&confPath, "conf", "", "default config path")
+}
+
+// Init init conf
+func Init() error {
+	if confPath != "" {
+		return local()
+	}
+	return remote()
+}
+
+func local() (err error) {
+	_, err = toml.DecodeFile(confPath, &Conf)
+	return
+}
+
+func remote() (err error) {
+	if client, err = conf.New(); err != nil {
+		return
+	}
+	if err = load(); err != nil {
+		return
+	}
+	go func() {
+		for range client.Event() {
+			log.Info("config reload")
+			if load() != nil {
+				log.Error("config reload error (%v)", err)
+			}
+		}
+	}()
+	return
+}
+
+func load() (err error) {
+	var (
+		s       string
+		ok      bool
+		tmpConf *Config
+	)
+	if s, ok = client.Toml2(); !ok {
+		return errors.New("load config center error")
+	}
+	if _, err = toml.Decode(s, &tmpConf); err != nil {
+		return errors.New("could not decode config")
+	}
+	*Conf = *tmpConf
+	return
+}
